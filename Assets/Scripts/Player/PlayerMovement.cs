75538c370884f83b4b1875e00bfd60e4edbf018b
Aleksandr <a.d.sizonenko@gmail.com>
2024-08-29 17:55:31 +0300

    Implemented player movement and rotation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,82 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private float speed = 25.0f;
-    [SerializeField] private float jumpSpeed = 8.0f;
-    [SerializeField] private float gravity = 20.0f;
-    [SerializeField] private float sensitivity = 5f;
-    private CharacterController _controller;
-    private float _horizontal, _vertical;
-    private float _mouseX, _mouseY;
-    private bool _jump;
-    
-    void Awake()
+    public class PlayerMovement : MonoBehaviour
     {
-        _controller = GetComponent<CharacterController>();
-        SetCursorLockState(false);
-    }
+        [SerializeField] private float speed = 25.0f;
+        private CharacterController _controller;
+        private float _horizontal, _vertical;
+        private bool _jump;
 
-    // screen drawing update - read inputs here
-    private void Update()
-    {
-        _horizontal = Input.GetAxis("Horizontal");
-        _vertical = Input.GetAxis("Vertical");
-        _mouseX = Input.GetAxis("Mouse X");
-        _mouseY = Input.GetAxis("Mouse Y");
-        _jump = Input.GetButton("Jump");
-        if (Input.GetKey(KeyCode.Escape))
+        void Awake()
         {
-            SetCursorLockState(true);
+            _controller = GetComponent<CharacterController>();
+            SetCursorLockState(false);
         }
-    }
-
-    // physics simulation update - apply physics forces here
-    private void FixedUpdate()
-    {
-        Vector3 moveDirection = Vector3.zero;
-
-        // is the controller on the ground?
-
-        // feed moveDirection with input.
-        moveDirection = new Vector3(_horizontal, 0, _vertical);
-        moveDirection = transform.TransformDirection(moveDirection);
-
-        // multiply it by speed.
-        moveDirection *= speed;
-
-        float turner = _mouseX * sensitivity;
-        if (turner != 0)
+        
+        private void Update()
         {
-            // action on mouse moving right
-            transform.eulerAngles += new Vector3(0, turner, 0);
+            _horizontal = Input.GetAxis("Horizontal");
+            _vertical = Input.GetAxis("Vertical");
+            _jump = Input.GetButton("Jump");
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                SetCursorLockState(true);
+            }
+            MovePlayer();
         }
 
-        float looker = -_mouseY * sensitivity;
-        if (looker != 0)
+       
+
+        private void MovePlayer()
         {
-            // action on mouse moving right
-            transform.eulerAngles += new Vector3(looker, 0, 0);
+            Vector3 moveDirection = Vector3.zero;
+            moveDirection = new Vector3(_horizontal, 0, _vertical);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            _controller.Move(moveDirection * Time.deltaTime);
         }
 
-        // apply gravity to the controller
-        moveDirection.y -= gravity * Time.deltaTime;
-
-        // make the character move
-        _controller.Move(moveDirection * Time.deltaTime);
-    }
-
-    private void SetCursorLockState(bool state)
-    {
-        if (!state)
+        private void SetCursorLockState(bool state)
         {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            return;
+            if (!state)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                return;
+            }
+
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
         }
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible = true;
     }
 }
---
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerRotation : MonoBehaviour
+    {
+        [SerializeField] private Camera playerCamera;
+        [SerializeField] private float sensitivity = 5f;
+        private float _mouseX;
+        private float _mouseY;
+        private Vector3 cameraRotate;
+        private Vector3 playerRotate;
+
+        void Update()
+        {
+            _mouseX = Input.GetAxis("Mouse X");
+            _mouseY = Input.GetAxis("Mouse Y");
+            RotatePlayer();
+        }
+
+        private void RotatePlayer()
+        {
+            cameraRotate = new Vector3(_mouseY* sensitivity, 0, 0);
+            playerRotate = new Vector3(0, -_mouseX * sensitivity, 0);
+            playerCamera.transform.eulerAngles = playerCamera.transform.eulerAngles - cameraRotate;
+            transform.eulerAngles = transform.eulerAngles - playerRotate;
+        }
+    }
+}
